ef31986db01736ebb86e04a2cad50d5e2283c505
Luis Hernandez <lahr0809@gmail.com>
2024-06-25 09:39:08 -0500

    add index api css2

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>API - Nutricare</title>
     <link rel="stylesheet" href="api/assets/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/style.css" type="text/css">
 </head>
 <body>
 
